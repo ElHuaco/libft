6feb76a301c4b79658d23cc34f13b8a2caf9804b
Alejandro Leon castell <aleon-ca@c1r5s4.42madrid.com>
2019-12-18 15:26:14 +0100

    Fixed strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: aleon-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 16:26:12 by aleon-ca          #+#    #+#             */
-/*   Updated: 2019/12/02 16:52:40 by aleon-ca         ###   ########.fr       */
+/*   Updated: 2019/12/16 19:13:15 by aleon-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,15 @@ char		*ft_strjoin(char const *s1, char const *s2)
 	{
 		lens1 = ft_strlen(s1);
 		lens2 = ft_strlen(s2);
-		j = malloc(sizeof(char) * (lens1 + lens2 + 1);
+		j = malloc(sizeof(char) * (lens1 + lens2 + 1));
 		if (j == 0)
 			return (0);
 		i = -1;
 		while (++i < lens1)
-			j[i] = *(cs1 + i);
+			j[i] = *((char *)s1 + i);
 		k = -1;
 		while (++k < lens2)
-			j[i + k] = *(cs2 + k);
+			j[i + k] = *((char *)s2 + k);
 		j[i + k] = '\0';
 		return (j);
 	}
