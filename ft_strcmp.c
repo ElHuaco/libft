57ed73a601624349538413cbee2f4aee79a088f5
Alejandro Leon castell <aleon-ca@c1r5s4.42madrid.com>
2020-02-04 12:17:41 +0100

    Changed strcmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,28 +6,24 @@
 /*   By: aleon-ca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/03 14:28:06 by aleon-ca          #+#    #+#             */
-/*   Updated: 2020/02/03 14:29:37 by aleon-ca         ###   ########.fr       */
+/*   Updated: 2020/02/04 12:15:52 by aleon-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_strcmp(const char *s1, const char *s2)
+int		ft_strcmp(char *s1, char *s2)
 {
 	size_t			i;
-	unsigned char	*cs1;
-	unsigned char	*cs2;
 
 	i = 0;
-	cs1 = (unsigned char *)s1;
-	cs2 = (unsigned char *)s2;
 	while (s1[i] || s2[i])
 	{
-		if (cs1[i] == cs2[i])
+		if (s1[i] == s2[i])
 			i++;
 		else
 		{
-			if (cs1[i] > cs2[i])
+			if (s1[i] > s2[i])
 				return (1);
 			else
 				return (-1);
